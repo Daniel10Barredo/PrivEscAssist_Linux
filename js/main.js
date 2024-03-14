14246fc8621cc1f1eacf7af0a561a7c646f482d6
Danny <102974995+Daniel10Barredo@users.noreply.github.com>
2024-03-14 15:39:37 +0100

    V0.1 Initial version

---
+++ js/main.js
@@ -0,0 +1,157 @@
+
+//Al cargar
+window.onload = function() {
+    //Preloader
+    $("#preloader").fadeOut(2000);
+    //Evantos de navegador
+    inputStyles();
+    $(".navbar-nav").find("input").change(inputStyles);
+    //Comprobar si hay un enlace interno
+    goToPage();
+    //Añade hook a links internos
+    $(".internal_link").on( "click",linkInternoHook);
+    //Imag hacker
+    hackerTransition();
+    //Al cambiar url
+    $(window).bind('popstate', function() {
+        goToPage();
+    });
+};
+
+//Comportamiento menu medium
+$('.sub-menu ul').hide();
+$(".submenu-custom").click(function () {
+    $(this).parent().children("ul").slideToggle("100");
+    $(this).find(".fa").toggleClass("fa-caret-up fa-caret-down");
+    $(this).find(".sub-tog").toggleClass("sub-tog-up sub-tog-down");
+});
+
+//Función tachado menus
+function inputStyles() {
+    $(".navbar-nav").find("input").each(function(){
+        if(this.checked){
+            $(this).parent().addClass("nav-selectedsub");
+        }else{
+            $(this).parent().removeClass("nav-selectedsub");
+        }
+    })
+    //Comprueba si todos los subitem están checkeados
+    $(".nav-item").each(function(){
+        var auxitem=true
+        $(this).find("input").each(function(){
+            auxitem &= this.checked;
+        })
+        if (auxitem){
+            $(this).addClass("nav-selectedsub");
+            if($(this).children("ul").is(':visible')){
+                $(this).children("ul").slideToggle("100");
+                $(this).find(".fa").toggleClass("fa-caret-up fa-caret-down");
+                $(this).find(".sub-tog").toggleClass("sub-tog-up sub-tog-down");
+            }
+        }else{
+            $(this).removeClass("nav-selectedsub");
+        }
+    })
+}
+
+/*Para boton de copy en snipets*/
+function addCopyButtons(){
+    // use a class selector if available
+    let blocks = document.querySelectorAll(".snipet_custom");
+    blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerHTML = '<i class="bi bi-clipboard"></i>';
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+        await copyCode(block, button);
+        });
+    }
+    });
+}
+async function copyCode(block, button) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+
+  await navigator.clipboard.writeText(text);
+
+  // visual feedback that task is completed
+  button.innerHTML = '<i class="bi bi-clipboard-check"></i>';
+
+  setTimeout(() => {
+    button.innerHTML = '<i class="bi bi-clipboard"></i>';
+  }, 700);
+}
+
+/*Función para cargar la página según link interno*/
+function goToPage() {
+    var hash = window.location.hash.replace("#","");
+    if(hash != ""){
+        var url = "./pages/"+hash
+        //Se realiza la petición a la URL
+        $.get(url+".html", function(data) {
+            //Si está en móvil pliega el menú
+            mobileClose();
+            //Se sobreescribe la información
+            $("#mainContent").slideUp(300,function() {
+                $(this).html(data).slideDown(300,function(){Rainbow.color();addCopyButtons();});
+                $("#hntext").html(hash.replace("/"," > "));
+                $("#homeNav").toggleClass("glitch_nav");
+                setTimeout(function() {
+                    $("#homeNav").toggleClass("glitch_nav");
+                }, 500);
+                $("#mainContent").toggleClass("glitch_main");
+                setTimeout(function() {
+                    $("#mainContent").toggleClass("glitch_main");
+                }, 500);
+                $("#logo").toggleClass("glitch_logo");
+                setTimeout(function() {
+                    $("#logo").toggleClass("glitch_logo");
+                }, 500);
+                $(this).find(".internal_link").on( "click",linkInternoHook);
+                SetCurrentPageInMenu();
+                document.title = "PrivEscAssist: "+hash.replace("/"," - ");
+              })
+        });
+    }
+}
+
+/*Hook click links internos*/
+function linkInternoHook() {
+    hash=this.getAttribute('href');
+    window.location=window.location.pathname+hash;
+}
+
+/*Cambiar estilo del link donde se encuentra*/
+function SetCurrentPageInMenu() {
+    for (var item of  $(".internal_link")) {
+        if(item.getAttribute("href") == window.location.hash){
+            item.classList.add("SelectedItem");
+        }
+        else{
+            item.classList.remove("SelectedItem");
+        }
+    }
+}
+
+
+/*En móvil, pliega menu al pulsar link*/
+function mobileClose() {
+    //Si está en modo móvil y el menú está desplegado
+    if($(".navbar-toggler").is(':visible') && $("#navbarResponsive").is(':visible')){
+        $("#navbarResponsive").removeClass("show");
+    }
+}
+
+/* Transición de imágen hacker */
+function hackerTransition() {
+    if(window.location.hash.replace("#","")==""){
+        $("#mainImag").attr("src","./media/imag/transition.gif").delay(2500).queue(function() {
+            $("#mainImag").attr("src","./media/imag/hacker.gif");
+            $(this).dequeue();
+         });
+    }
+}
